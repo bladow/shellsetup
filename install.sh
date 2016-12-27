9c9c03a14dc270951b6d9468fc6abf37ee379b5e
[cowboy] <cowboy@redacted.com>
2016-12-27 12:17:17 -0500

    Install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+## Copy config files
+cp .bash_profile ~/
+cp .git-prompt.sh ~/
+cp .gitconfig ~/
+cp .docker_alias ~/
+
+## Install vgod's VIM setup.
+curl -o - https://raw.github.com/vgod/vimrc/master/auto-install.sh | sh
+
+## Install liquidprompt
+git clone https://github.com/nojhan/liquidprompt.git ~/.liquidprompt
+
+## Install SSH Config
+git archive --remote=git@bitbucket.org:notacow/ssh_config.git HEAD config | tar -x && mv config ~/.ssh/
+
