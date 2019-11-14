12ef786e9886a18f393c0d7950f51b864cee0253
Garrett Bladow <gbladow@dragos.com>
2019-11-14 16:43:45 -0500

    New additions for Catalina

--- install.sh
+++ install.sh
@@ -3,13 +3,22 @@
 cp .bash_profile ~/
 cp .git-prompt.sh ~/
 cp .gitconfig ~/
+cp .gitignore ~/
 cp .docker_alias ~/
+
+## Install CLI tools
+xcode-select --install
+## Configure git
+git config --global core.excludesfile ~/.gitignore
 
 ## Install vgod's VIM setup.
 curl -o - https://raw.github.com/vgod/vimrc/master/auto-install.sh | sh
 
 ## Install liquidprompt
 git clone https://github.com/nojhan/liquidprompt.git ~/.liquidprompt
+
+## Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
 
 ## Install SSH Config
 
